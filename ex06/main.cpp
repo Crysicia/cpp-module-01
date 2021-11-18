11b2b5c695b0513c35806321408dd2513d27c30d
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-11-18 18:29:17 +0100

    Add explicit fallthrough

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -4,13 +4,12 @@ int main(int argc, char *argv[]) {
 	Karen karen;
 	if (argc != 2) { std::cout << "Invalid arguments number, expecting 1 got " << argc - 1 << std::endl; return 1; }
 
-
 	switch(karen.getLevelIndex(argv[1])) {
 		case UNKNOWN: std::cout << "[ Probably complaining about insignificant problems ]" << std::endl; break;
-		case DEBUG: karen.complain("DEBUG");
-		case INFO: karen.complain("INFO");
-		case WARNING: karen.complain("WARNING");
-		case ERROR: karen.complain("ERROR"); break;
+		case DEBUG: karen.complain("DEBUG"); __attribute__ ((fallthrough));;
+		case INFO: karen.complain("INFO"); __attribute__ ((fallthrough));;
+		case WARNING: karen.complain("WARNING"); __attribute__ ((fallthrough));;
+		case ERROR: karen.complain("ERROR");
 	}
 	return 0;
 }
